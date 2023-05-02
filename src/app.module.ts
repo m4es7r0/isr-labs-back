23fb6335c7fc4d8ca90bc08dd3f210331bb3963e
Dmytro <dmitrymusiichenko@gmail.com>
2023-05-02 23:37:45 +0300

    1.0.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,9 +7,8 @@ import { User, UserSchema } from './schems/user-data.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://isrlabsforstudy:vSdwDdxNNLXOOsPC@db.er1kgiy.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot({ envFilePath: '.env.local' }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [process.env.CLIENT_URL],
+  });
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
