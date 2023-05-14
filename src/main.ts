e61efb8c056077f71ed18b648539d9629416c3fd
Dmitry Musiichenko <41909013+m4es7r0@users.noreply.github.com>
2023-05-14 14:59:06 +0300

    ...

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      process.env.CLIENT_URL || 'https://isr-labs-back.vercel.app/registration',
-    ],
+    origin: [process.env.CLIENT_URL || 'https://isr-labs.netlify.app/'],
   });
   await app.listen(process.env.PORT || 4000);
 }
