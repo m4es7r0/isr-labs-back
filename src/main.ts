29e9975fb13dc9a586082428bc7ded117792fb94
Dmitry Musiichenko <dmitrymusiichenko@gmail.com>
2023-05-14 16:13:53 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: [process.env.CLIENT_URL],
+      origin: [process.env.CLIENT_URL, "https://isr-labs.netlify.app"],
     },
   });
   await app.listen(process.env.PORT || 4000);
