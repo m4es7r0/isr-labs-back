e7d1346db2753c89e852669dc96348f4c8fc323d
Dmitry Musiichenko <41909013+m4es7r0@users.noreply.github.com>
2023-05-14 16:02:45 +0300

    cors set

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [process.env.CLIENT_URL],
+    },
+  });
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
