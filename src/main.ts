ff658d4876e082d4fdabdee472001d2011728e27
Dmitry Musiichenko <41909013+m4es7r0@users.noreply.github.com>
2023-05-14 15:12:19 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.CLIENT_URL || 'https://isr-labs.netlify.app/'],
+    origin: ['https://isr-labs.netlify.app/'],
   });
   await app.listen(process.env.PORT || 4000);
 }
